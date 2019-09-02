3c31e2f7e8cad0c6907d69f224f7b5cc8e4e8c57
tehske <51287266+tehske@users.noreply.github.com>
2019-09-02 22:38:39 +0200

    All classes added (some out of order)

--- src/data.js
+++ src/data.js
@@ -1,73 +1,118 @@
 export const classes = {
   brute: {
     total: 29,
-    starting: 13
+    starting: 13,
+	order: [7,8,9,12,16,17,18,21,23,26,1,14,20,5,28,2,3,19,25,15,22,4,
+	        10,13,24,11,29,6,27]
   },
   cragheart: {
     total: 30,
-    starting: 14
+    starting: 14,
+    order: [1,2,8,9,11,12,18,21,25,27,30,14,15,20,13,28,4,7,16,24,22,
+			29,6,10,5,19,17,26,3,23]
   },
   mindthief: {
     starting: 13,
-    total:29
+    total:29,
+    order: [6,7,8,11,13,16,17,22,25,26,9,20,28,12,29,1,24,3,19,10,15,
+			2,4,21,27,5,23,14,18]
   },
   scoundrel: {
     total: 28,
-    starting: 12
+    starting: 12,
+    order: [1,6,14,15,17,18,23,24,26,16,22,25,7,12,5,10,8,9,27,4,2,3,
+			19,20,21,13,11,28]
   },
   spellweaver: {
     total: 27,
-    starting: 11
+    starting: 11,
+    order: [9,10,13,14,18,21,22,23,1,6,16,11,17,4,7,12,24,3,8,15,20,
+			25,26,5,27,2,19]
   },
   tinkerer: {
     total: 31,
-    starting: 15
+    starting: 15,
+    order: [7,11,13,17,21,22,23,26,28,29,30,31,9,12,27,5,19,10,15,3,
+			16,6,8,18,24,14,25,1,20,2,4]
   },
-  sun: {
-    locked: true,
-    total: 30,
-    starting: 14
+  diviner: {
+    total: 28,
+    starting: 12,
+    order: [2,6,9,12,20,21,24,25,28,3,13,19,17,26,4,15,7,23,11,27,5,
+			14,8,16,1,10,18,22]
   },
   'angry-face': {
     locked: true,
     total: 31,
     starting: 15,
-    order: [22, 4, 8, 30, 21, 17, 6, 13, 11, 27, 1, 28, 25, 29, 12, 7,
-            23, 5, 20, 26, 10, 15, 31, 18, 2, 14, 3, 24, 9, 19, 16]
+    order: [2,5,6,9,11,14,16,18,20,22,29,30,3,7,21,15,31,12,28,4,19,10,
+			25,13,27,8,23,17,26,1,24]
+  },
+  circles: {
+	locked: true,
+    total: 28,
+    starting: 12,
+    order: [1,2,3,9,17,18,25,26,28,8,15,27,6,10,20,24,5,16,4,23,7,12,
+			19,22,14,21,11,13]
+  },
+  sun: {
+    locked: true,
+    total: 30,
+    starting: 14,
+    order: []
   },
   triangle: {
     locked: true,
     total: 29,
     starting: 13,
-    order: [22, 21, 11, 23, 25, 26, 15, 16, 14, 12, 17, 2, 7, 1, 5, 3,
-            4, 20, 13, 18, 29, 10, 24, 27, 19, 28, 6, 9, 8]
+    order: []
   },
   'triple-spears': {
     locked: true,
     total: 28,
     starting: 12,
-    order: [21, 13, 27, 15, 12, 10, 4, 11, 6, 7, 26, 20, 17, 2, 5, 25,
-            28, 9, 19, 24, 3, 16, 23, 18, 14, 8, 22, 1]
+    order: []
   },
   cthulhu: {
     locked: true,
     total: 30,
     starting: 14,
-    order: [28, 13, 18, 22, 4, 26, 8, 12, 17, 11, 20, 9, 29, 6, 27, 19,
-            25, 10, 24, 16, 1, 30, 23, 5, 2, 3, 21, 14, 15, 7]
+    order: []
   },
   'lightning-bolt': {
     locked: true,
     total: 29,
     starting: 13,
-    order: [22, 18, 26, 10, 15, 2, 8, 9, 16, 4, 29, 17, 20, 21, 5, 25,
-            12, 14, 24, 23, 13, 11, 28, 6, 7, 3, 1, 19, 27]
+    order: []
   },
   'music-note': {
     locked: true,
     total: 28,
     starting: 12,
-    order: [16, 7, 21, 27, 2, 25, 24, 20, 11, 23, 28, 15, 22, 4, 9, 6,
-            8, 10, 12, 13, 18, 17, 14, 1, 26, 5, 3, 19]
+    order: []
+  },
+  eclipse: {
+    locked: true,
+    total: 28,
+    starting: 12,
+    order: []
+  },
+  saw: {
+    locked: true,
+    total: 31,
+    starting: 15,
+    order: []
+  },
+  'two-mini': {
+    locked: true,
+    total: 30,
+    starting: 14,
+    order: []
+  },
+  what: {
+    locked: true,
+    total: 30,
+    starting: 14,
+    order: []
   }
 }
